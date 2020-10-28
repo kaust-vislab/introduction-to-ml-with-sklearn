1bc4e98f93294240d316957c87f8a0ad3ac9046e
David R. Pugh <drpugh@protonmail.com>
2020-10-28 13:15:31 +0300

    Initial commit

---
+++ bin/create-conda-env.sh
@@ -0,0 +1,7 @@
+#!/bin/bash --login
+
+# create the conda environment
+export ENV_PREFIX=$PWD/env
+conda env create --prefix $ENV_PREFIX --file environment.yml --force
+conda activate $ENV_PREFIX
+source postBuild
